442dd03144f4237561e22fcec6e1b46947f75f87
Lalith143kl <lalithnarayanavuppala@gmail.com>
2023-03-29 21:05:11 -0500

    added files

--- routes/computation.js
+++ routes/computation.js
@@ -11,20 +11,20 @@ router.get('/', function(req, res, next) {
 if(req.query.x ==undefined ) {
   x=Math.floor(Math.random()*10);
   y=Math.fround(Math.random());
-let y1=Math.acosh(x);
-let y2=Math.log1p(y);
-let y3=Math.tan(x);
-  res.send('Math.acosh() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.log1p() applied to '+y+' is '+y2+"<br>"+'Math.tan()  applied to '+x+' is'+y3); 
+let y1=Math.atan(x);
+let y2=Math.exp(y);
+let y3=Math.expm1(x);
+  res.send('Math.atan() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.exp() applied to '+y+' is '+y2+"<br>"+'Math.expm1()  applied to '+x+' is'+y3); 
 
 }
 else{
 
   x = req.query.x;
-  let y1=Math.acosh(x);
-  let y2=Math.log1p(x);
-  let y3=Math.tan(x);
+  let y1=Math.atan(x);
+  let y2=Math.exp(x);
+  let y3=Math.expm1(x);
 
-  res.send('Math.acosh() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.log1p() applied to '+y+' is '+y2+"<br>"+'Math.tan()  applied to '+x+' is'+y3);
+  res.send('Math.atan() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.exp() applied to '+y+' is '+y2+"<br>"+'Math.expm1()  applied to '+x+' is'+y3);
 }
 });
 
--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'lalit vuppala' });
+  res.render('mydata', { title: 'lalith vuppala' });
 });
 
 module.exports = router;
